efdb19964d9a36dafd32e4cb4b86cd8e00a89a1c
Marcelo Ramos Correia <teljp37@gmail.com>
2024-03-11 11:24:23 -0300

    Criação da pasta extras
    Aulas do canal Programação Web, do Prof. Dimitri Teixeira

---
+++ extras/canal_programacaoweb/tipos-de-saida/ex01.js
@@ -0,0 +1,8 @@
+//Inserção de texto no documento HTML usando innerHTML:
+document.getElementById("texto").innerHTML ="Meu primeiro texto <strong>JS!</strong>";
+
+//Outra forma de saída no JS:
+alert("Olá => Isto é um alerta (window.alert())");
+
+//Por console.log:
+console.log("Mostrando este texto usando console.log - não aparece no documento HTML");
