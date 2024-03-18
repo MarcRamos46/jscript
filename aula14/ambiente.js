ce633c7560a0dbe4a5aad8d7b5d7821bc8bf9031
Marcelo Ramos Correia <teljp37@gmail.com>
2024-03-18 19:01:09 -0300

    Aula14
    Exercícios com FOR e aprendendo a usar o Debug no VSCode.

---
+++ aula14/ambiente.js
@@ -0,0 +1,11 @@
+console.log("Exercícios com o FOR:");
+console.log("Exibe apenas os números pares entre 1 e 20\n")
+
+for (let c = 1; c <= 5; c ++) {
+    if (c % 2 == 0){
+        console.log(` => ${c} é PAR`);
+    } else {
+        console.log("> pula <");
+}
+}
+console.log("\nFim do Programa");
