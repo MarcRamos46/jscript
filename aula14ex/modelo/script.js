32566130d5e269963a93ff66cb389aa79144b8e3
Marcelo Ramos Correia <teljp37@gmail.com>
2024-03-24 13:36:58 -0300

    Criação de contagem.html e contagem.js
    É o exercício 17 de aula14ex

--- aula14ex/modelo/script.js
+++ aula14ex/modelo/script.js
@@ -1,39 +1,20 @@
-function contar(){
+function calcular(){
     
-    let tn1 = window.document.getElementById('txtn1') //Início
-    let tn2 = window.document.getElementById('txtn2') //Fim 
-    let tn3 = window.document.getElementById('txtn3')//Passo
-    let res = window.document.getElementById('res') //Div onde será inserido o resultado
+    let num = window.document.getElementById('txtnúmero') 
+    let tab = window.document.getElementById('seltab')
 
-    if (tn1.value.length == 0 || tn2.value.length == 0 || tn3.value.length == 0) { // 1ª Validação
-        res.innerHTML ="[ERRO]! Impossível contar."
+    if (num.value.length == 0){
+        
+        alert('Por favor, digite um número.')
+    
     } else {
-       
-        res.innerHTML = 'Contando: <br>'//Posso usar <br> para quebrar linha porque foi usado o método .innerHTML
-        
-        let início = Number(tn1.value) 
-        let fim = Number(tn2.value)
-        let passo = Number(tn3.value)
-
-    if (passo == 0) { // 2ª Validação
-        window.alert('Passo INVÁLIDO! Considerando Passo = 1')
-        res.innerHTML = 'Considerando Passo = 1: <br>'
-        passo = 1
-    }
-    if (início < fim) { 
-            //Contagem crescente
-            while (início <= fim) {
-                res.innerHTML += `${início} \u{1F449} `
-                início += passo 
-            }
-        } else {
-            //Contagem descrescente
-            while (início >= fim) {
-                res.innerHTML += `${início} \u{1F449} `
-                início -= passo
-            }
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for (let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${i} = ${n * i}`
+            item.value = `tab${i}` //Linha de código importante para outras linguagens como PHP, mas não tanto pra JS. Porém, importante a inserção desta linha, para que seja identificado o valor específico de cada item.
+            tab.appendChild(item)
         }
-        res.innerHTML += `\u{1F3C1}`
-    }
-        
+    }    
 }
